2d53992d6f763a96bb414c23a7ad77d9105798b2
Trish Ang <feeshsauce@gmail.com>
2011-05-21 11:32:58 -0700

    Adding the Plone logo tag... maybe?

--- ploneconf2011/theme/skins/ploneconf2011_theme_styles/ploneconf.js
+++ ploneconf2011/theme/skins/ploneconf2011_theme_styles/ploneconf.js
@@ -3,4 +3,22 @@
      function() { jq(this).addClass("hover"); },
      function() { jq(this).removeClass("hover"); }
    );
+
+   jq("ul.thumb li").hover(function() {
+	jq(this).css({'z-index' : '10'}); /*Add a higher z-index value so this image stays on top*/ 
+	jq(this).find('div').addClass("hover").stop() /* Add class of "hover", then stop animation queue buildup*/
+		.animate({
+			marginTop: '-100px', /* The next 4 lines will vertically align this image */
+			marginLeft: '-110px'
+		}, 200); /* this value of "200" is the speed of how fast/slow this hover animates */
+
+	} , function() {
+	jq(this).css({'z-index' : '0'}); /* Set z-index back to 0 */
+	jq(this).find('div').removeClass("hover").stop()  /* Remove the "hover" class , then stop animation queue buildup*/
+		.animate({
+			marginTop: '0', /* Set alignment back to default */
+			marginLeft: '0'
+		}, 400);
+});
+
 }); })(jQuery);
