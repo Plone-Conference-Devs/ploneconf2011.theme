2cf67f3b5618da057c37faf33cecc8264ff9e1e6
Chrissy Wainwright <chrissy@c-vm.sixfeetup.com>
2011-06-04 16:35:16 -0400

    changed JS for site background so it isn't fixed, can fade to black. Adjusted position and site of the global nav to fit in column

--- ploneconf2011/theme/skins/ploneconf2011_theme_styles/ploneconf.js
+++ ploneconf2011/theme/skins/ploneconf2011_theme_styles/ploneconf.js
@@ -1,9 +1,11 @@
 (function($) { $(function() {
+   // hover class for dropdowns
    jq("#portal-globalnav li").hover(
      function() { jq(this).addClass("hover"); },
      function() { jq(this).removeClass("hover"); }
    );
 
+   // speaker popups
    jq("ul.spkr li").hover(function() {
 	jq(this).css({'z-index' : '10'}); /*Add a higher z-index value so this image stays on top*/ 
 	jq(this).find('div').addClass("hover").stop() /* Add class of "hover", then stop animation queue buildup*/
@@ -19,6 +21,25 @@
 			marginTop: '0', /* Set alignment back to default */
 			marginLeft: '0'
 		}, 400);
-});
+    });
+    
+    // background stretch
+	jq(window).resize(bg);
+    function bg() {
+        var imgWidth = 1088, 
+            imgHeight = 858,
+            imgRatio = imgWidth / imgHeight,
+            bgWidth = jq(document).width(),
+            bgHeight = bgWidth / imgRatio;
+
+            jq('body').css({backgroundPosition: 'top center'});
+            if (jq(document).width() < imgWidth && jq(document).height() < imgHeight && jq(document).height() > bgHeight) {
+                jq('body').css({backgroundSize: 'auto 100%'});
+            } else {
+                jq('body').css({backgroundSize: '100% auto'});
+            }
+    }
+    bg();
+    jq('body').css({backgroundRepeat: 'repeat-x'});
 
 }); })(jQuery);
