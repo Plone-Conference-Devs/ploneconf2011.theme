a9ac62dbfed5a274ca34fa1f1e578e3571f0c92c
Elizabeth Leddy <elizabeth.leddy@gmail.com>
2011-06-09 18:24:03 -0700

    add images by default

--- ploneconf2011/theme/setuphandlers.py
+++ ploneconf2011/theme/setuphandlers.py
@@ -1,3 +1,7 @@
+import StringIO
+import transaction
+
+
 def setupVarious(context):
 
     # Ordinarily, GenericSetup handlers check for the existence of XML files.
@@ -13,3 +17,52 @@ def setupVarious(context):
     front = site.get('front-page')
     if front:
         front.layout = 'home_page_view'
+        
+        # set the default content so that we can see what this will look like
+        # but don't override if its already been set
+        if front.getText().count("Make it your own"):
+            front.setText("""San Francisco, California TITLE HERE
+It's time to start planning your pilgrimage to the annual Plone Conference in San Francisco. We're building an action-packed lineup of training, talks, and sprints for you and 400 of your closest friends. Reunite with old friends and finally put a face to that IRC handle this November by registering now!""")
+            front.setDescription("")
+
+    # set the initial images
+    slide = site.get('slideshow-folder')
+    if slide:
+        
+        # only add if this is a first time setup
+        if not slide.getChildNodes():
+            # XXX: this would be fun if it was reading a metadata file instead :)
+            #       dreams for another day I guess...
+            addMe = [
+                        ("ss-sprint.jpg", "Contribute Back", "Sprint | November 7-8"), 
+                        ("ss-conf.jpg", "Catch Up", "Conference | November 3-6"), 
+                        ("ss-training.jpg", "Get Learned", "Training | November 1-2"), 
+                    ]
+            for (location, title, description) in addMe:
+                
+                imageId = location.split(".")[0]
+                
+                imageData = context.readDataFile(location, 'images')
+                if imageData:   
+                    transaction.begin() # in case this list gets huge
+                    # open up the resources and get the binary shizzle into some
+                    # stringIO field for passing onto THE machinery
+                    imageFile = StringIO.StringIO()
+                    imageFile.write(imageData)
+                    
+                    slide.invokeFactory(id=imageId, type_name='Image')
+                    newImage = slide.get(imageId)
+                    newImage.setImage(imageFile)
+                    imageFile.close()
+                    
+                    
+                    newImage.setTitle(title)
+                    newImage.setDescription(description)
+                    newImage.reindexObject() #required for title to persist
+                    transaction.commit()
+                    
+                    
+                
+
+
+            
